9a50df8c26d7e35264c6c1eaead2c2f51d8bb0a7
rajpranit <rajpranit1207@gmail.com>
2022-11-03 09:47:24 +0530

    Writing more flexible routing code

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,13 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 import NoQuotesFound from "../components/quotes/NoQuotesFound";
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
+  console.log(match);
+
   const DummyData = [
     { id: "q1", author: "Ayush", text: "This is my first quote of the day." },
     { id: "q2", author: "Mishra", text: "This is my second quote of the day." },
@@ -16,7 +19,7 @@ const QuoteDetail = () => {
 
   if (!quote) {
     return (
-      <Route path={`/quotes/${params.quoteId}`}>
+      <Route path={`${match.path}`}>
         <NoQuotesFound></NoQuotesFound>
       </Route>
     );
@@ -25,6 +28,15 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
+
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
